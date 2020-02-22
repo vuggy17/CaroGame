415dc68c480593892730f54c064b69f1ecc3ae8a
KhuongDuyVD <ramenxlevel@gmail.com>
2020-02-22 22:54:06 +0700

    đang làm dở hàm checkwin

--- Model.h
+++ Model.h
@@ -5,6 +5,10 @@ void ResetData();
 int ExitGame();
 int CheckBoard(int pX, int pY);
 int TestBoard();
-int BoardFull();
-int WinGame();
+bool BoardFull();
+int check_ngang(int a, int b);
+//int check_doc(int a, int b);
+//int check_cheophai(int a, int b);
+//int check_cheotrai(int a, int b);
+bool WinGame();
 #endif
